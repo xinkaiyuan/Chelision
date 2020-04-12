2ab23428916c9be80796f16a8dd033a75dc27d87
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-04-12 10:40:37 +0000

    Update pt-PT.coffee

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,4 +1,6 @@
 import { PassThrough } from "stream";
 import type { EntryContext } from "@remix-run/node";
 import { Response } from "@remix-run/node";
-import { RemixServer } from "@rem
+import { RemixServer } from "@remix-run/react";
+import isbot from "isbot";
+import { renderToPipeable
