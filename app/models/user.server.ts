a22561ff11d484f7480ea81bbaf485f2a3fde19f
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-06-14 21:44:47 +0000

    GUI Updates

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -1,4 +1,8 @@
 import type { Password, User } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
-import { prisma } from "~/db.server"
+import { prisma } from "~/db.server";
+
+export type { User } from "@prisma/client";
+
+expo
