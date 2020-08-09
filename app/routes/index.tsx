f995f161c655b8befb276faf45e05b9de1674315
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-08-09 16:19:04 +0000

    Adjust dependencies

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,4 +5,5 @@ import { useOptionalUser } from "~/utils";
 
 export const meta: V2_MetaFunction = () => [{ title: "Remix Notes" }];
 
-export defa
+export default function Index() {
+  const user = useOptionalUser(
