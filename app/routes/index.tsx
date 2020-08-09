5261c72f798451b8eb16036cd065bb1d65dad61e
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-08-09 10:53:14 +0000

    better indentation

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,4 +3,6 @@ import { Link } from "@remix-run/react";
 
 import { useOptionalUser } from "~/utils";
 
-export const meta
+export const meta: V2_MetaFunction = () => [{ title: "Remix Notes" }];
+
+export defa
