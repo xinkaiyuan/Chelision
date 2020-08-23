a4d4fc556d0e730512dcba0ca9a12908da3814f1
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-08-23 08:55:17 +0000

    Maven artifacts for Local Messaging version 1.1.7-SNAPSHOT

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -10,4 +10,5 @@ export default function Index() {
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
       <div className="relative sm:pb-16 sm:pt-8">
-        <div className="mx-auto max-w-7x
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="relative shadow-x
