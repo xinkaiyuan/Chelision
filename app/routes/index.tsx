f668f4faadc45983d4542cfa7cf9a9e4f028b836
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-08-30 13:06:24 +0000

    English version

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,4 +12,6 @@ export default function Index() {
       <div className="relative sm:pb-16 sm:pt-8">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-            <div className="absolute 
+            <div className="absolute inset-0">
+              <img
+                className="h-ful
