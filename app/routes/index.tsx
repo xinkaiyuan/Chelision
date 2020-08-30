2246da4f814543e0c2a3e30194dc233da199718a
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-08-30 22:39:58 +0000

    Update AC-Ontology.xml

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -15,4 +15,4 @@ export default function Index() {
             <div className="absolute inset-0">
               <img
                 className="h-full w-full object-cover"
-                src="https://user-images.gith
+                src="https://user-images.githubusercontent.com/1500684/157774694-99820c51-8165-4908-a031
