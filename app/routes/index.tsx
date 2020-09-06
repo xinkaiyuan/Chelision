acb754241502417561bc2e4a95bb17a16c5c26cf
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-09-06 10:23:36 +0000

    updated configs

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -17,4 +17,5 @@ export default function Index() {
                 className="h-full w-full object-cover"
                 src="https://user-images.githubusercontent.com/1500684/157774694-99820c51-8165-4908-a031-34fc371ac0d6.jpg"
                 alt="Sonic Youth On Stage"
-  
+              />
+              <div className="absolute inset
