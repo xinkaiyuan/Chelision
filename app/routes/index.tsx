399348bd12e17c99278a8b74642258ed0a9d836e
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-09-06 18:50:30 +0000

    Refactored code.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -18,4 +18,5 @@ export default function Index() {
                 src="https://user-images.githubusercontent.com/1500684/157774694-99820c51-8165-4908-a031-34fc371ac0d6.jpg"
                 alt="Sonic Youth On Stage"
               />
-              <div className="absolute inset
+              <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
+           
