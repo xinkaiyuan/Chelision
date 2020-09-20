c41ca584f2d715cb1ca7965065324976f1dfb45e
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-09-20 14:55:08 +0000

    Updated icon

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -22,4 +22,6 @@ export default function Index() {
             </div>
             <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="block uppercase text
+                <span className="block uppercase text-yellow-500 drop-shadow-md">
+                  Indie Stack
+       
