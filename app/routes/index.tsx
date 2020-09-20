b70a65598dc8d3371950fbf727011a1213a5bded
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-09-20 09:26:46 +0000

    submod

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -21,4 +21,4 @@ export default function Index() {
               <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
             </div>
             <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
-         
+              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8x
