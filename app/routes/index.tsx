3924543fd5340d7b8feea72da1d08da321b06272
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-10-09 15:56:53 +0000

    heroku changes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -24,4 +24,6 @@ export default function Index() {
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
                 <span className="block uppercase text-yellow-500 drop-shadow-md">
                   Indie Stack
-       
+                </span>
+              </h1>
+              <p className="mx-a
