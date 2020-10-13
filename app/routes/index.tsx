b4634059b3d23c45e57d349a9e24ec03651a812f
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-10-13 10:06:43 +0000

    test++

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -30,4 +30,6 @@ export default function Index() {
                 Check the README.md file for instructions on how to get this
                 project deployed.
               </p>
-              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w
+              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+                {user ? (
+   
