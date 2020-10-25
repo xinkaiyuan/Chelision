06ce32076d195ed0e38529612fc4625a4b7a41ba
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-10-25 11:16:07 +0000

    add contents

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -34,4 +34,4 @@ export default function Index() {
                 {user ? (
                   <Link
                     to="/notes"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base fo
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:p
