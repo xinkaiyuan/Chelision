6bbab4b95b4e045ff4ab53baa818e0dfd89d3fae
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-10-25 19:35:01 +0000

    Open Access csv

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -34,4 +34,6 @@ export default function Index() {
                 {user ? (
                   <Link
                     to="/notes"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:p
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+                  >
+                    View Note
