3b46b9de6c8210eb28b023a01cdf0268be55bb9b
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-10-25 19:36:11 +0000

    Delete default.html

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -36,4 +36,6 @@ export default function Index() {
                     to="/notes"
                     className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
                   >
-                    View Note
+                    View Notes for {user.email}
+                  </Link>
+  
