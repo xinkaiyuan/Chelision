920fa2cb096da68e883510744427ae36bf690e00
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-11-08 21:35:01 +0000

    Update config-client-dev.properties

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -42,4 +42,4 @@ export default function Index() {
                   <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                     <Link
                       to="/join"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py
+                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hove
