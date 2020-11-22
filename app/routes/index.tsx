1150efa95889aa32d17a46391a83d01126e4548b
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-11-22 22:10:14 +0000

    minor test update

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -48,4 +48,5 @@ export default function Index() {
                     </Link>
                     <Link
                       to="/login"
-                      className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 
+                      className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
+           
