319eeb7f21db78dd18993ddf9ec2a85980c4abd3
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-12-06 13:48:43 +0000

    NPE fixed

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -57,4 +57,6 @@ export default function Index() {
               </div>
               <a href="https://remix.run">
                 <img
-                  src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544
+                  src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544-4b69-96d6-d3bcc33fc76a.svg"
+                  alt="Remix"
+         
