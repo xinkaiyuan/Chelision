5a5b104928695f489f47f1558cd04927693773b1
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-12-08 13:03:11 +0000

    Add travis build file

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -60,4 +60,7 @@ export default function Index() {
                   src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544-4b69-96d6-d3bcc33fc76a.svg"
                   alt="Remix"
                   className="mx-auto mt-16 w-full max-w-[12rem] md:max-w-[16rem]"
-     
+                />
+              </a>
+            </div>
+      
