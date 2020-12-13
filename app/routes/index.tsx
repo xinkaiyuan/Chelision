c29d3d7a8b6200c281b3a8c5e1b43b0cf32dd23f
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2020-12-13 16:48:34 +0000

    add Travis CI support

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -66,4 +66,5 @@ export default function Index() {
           </div>
         </div>
 
-        <div className="mx-auto max-w-7xl py-2 px-4 sm:px-
+        <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
+          <div className="mt-6 flex flex-wrap justify-center gap-8
