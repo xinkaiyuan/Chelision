cef0a5f87b70f8c49e3b6c94e66697519b1029d4
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-01-17 05:37:43 +0000

    Release v2.2.2

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -85,4 +85,6 @@ export default function Index() {
                 href: "https://prisma.io",
               },
               {
-                src: "https://user-images.githubusercontent.com/1500684/157764276-a516a239-e377-4a20-b44a
+                src: "https://user-images.githubusercontent.com/1500684/157764276-a516a239-e377-4a20-b44a-0ac7b65c8c14.svg",
+                alt: "Tailwind",
+                hr
