5d2f0d54927d415750b449e0c5e7f9aaef15530d
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-01-24 10:06:31 +0000

    Create index.php

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -92,4 +92,7 @@ export default function Index() {
               {
                 src: "https://user-images.githubusercontent.com/1500684/157764454-48ac8c71-a2a9-4b5e-b19c-edef8b8953d6.svg",
                 alt: "Cypress",
-                href: "http
+                href: "https://www.cypress.io",
+              },
+              {
+      
