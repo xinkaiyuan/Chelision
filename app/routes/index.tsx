ecf1c7eb92c5d3338269db9f3e889b51940c476b
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-01-31 08:02:26 +0000

    directory changes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -96,4 +96,6 @@ export default function Index() {
               },
               {
                 src: "https://user-images.githubusercontent.com/1500684/157772386-75444196-0604-4340-af28-53b236faa182.svg",
- 
+                alt: "MSW",
+                href: "https://mswjs.io",
+  
