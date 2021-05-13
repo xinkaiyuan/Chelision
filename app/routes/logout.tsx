768bf705b5ad75306797b59aaa48a7d49e7cc524
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-05-13 17:24:55 +0000

    dsdf

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,4 +1,6 @@
 import type { ActionArgs } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 
-import { logout } from
+import { logout } from "~/session.server";
+
+export async function action({ request }: ActionA
