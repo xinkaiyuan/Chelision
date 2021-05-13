a6c76e9a67d29e9a603ab059bc83c6bae5374faa
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-05-13 20:06:00 +0000

    better formatting

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -3,4 +3,8 @@ import { redirect } from "@remix-run/node";
 
 import { logout } from "~/session.server";
 
-export async function action({ request }: ActionA
+export async function action({ request }: ActionArgs) {
+  return logout(request);
+}
+
+export async function loade
