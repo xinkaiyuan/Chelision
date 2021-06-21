afe00dc83136059a97ef65206a7bf239c16436a3
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-06-21 18:49:55 +0000

    [CLEANUP] updated versions via release version merger

--- app/routes/notes/$noteId.tsx
+++ app/routes/notes/$noteId.tsx
@@ -39,3 +39,7 @@ export default function NoteDetailsPage() {
           type="submit"
           className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
         >
+          Delete
+        </button>
+      </Form>
+    </di
