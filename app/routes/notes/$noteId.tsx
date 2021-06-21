30ee100cf46777d0d0e8d8c67f75e5cae585609a
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-06-21 17:25:21 +0000

    Add MANIFEST.in

--- app/routes/notes/$noteId.tsx
+++ app/routes/notes/$noteId.tsx
@@ -37,4 +37,5 @@ export default function NoteDetailsPage() {
       <Form method="post">
         <button
           type="submit"
-          className="rounded bg-blue-500  py-2 px-4 t
+          className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+        >
