d3db1fd7f35741a78088d8d7befe12793cd8f515
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-06-28 16:15:20 +0000

    NR-114 : Live Blog back end

--- app/routes/notes/index.tsx
+++ app/routes/notes/index.tsx
@@ -4,4 +4,7 @@ export default function NoteIndexPage() {
   return (
     <p>
       No note selected. Select a note on the left, or{" "}
-      <Link to="new" className="text-blue-50
+      <Link to="new" className="text-blue-500 underline">
+        create a new note.
+      </Link>
+  
