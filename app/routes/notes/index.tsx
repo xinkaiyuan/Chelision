718e4f02f5ffea64c8b6dbfe83767544722fd058
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-06-28 06:41:43 +0000

    [INTERNAL][TEST] sap.uxap ObjectPageIconTabBar: reference images changed

--- app/routes/notes/index.tsx
+++ app/routes/notes/index.tsx
@@ -3,4 +3,5 @@ import { Link } from "@remix-run/react";
 export default function NoteIndexPage() {
   return (
     <p>
-      No note selected. Select a note o
+      No note selected. Select a note on the left, or{" "}
+      <Link to="new" className="text-blue-50
