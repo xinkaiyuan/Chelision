75eb90f38d40dcaffc4dd4488a14cf80c5e38346
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-07-02 09:47:26 +0000

    USB: OHCI: don't lose track of EDs when a controller dies

---
+++ app/routes/notes.tsx
@@ -0,0 +1,25 @@
+
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+
+import { requireUserId } from "~/session.server";
+import { useUser } from "~/utils";
+import { getNoteListItems } from "~/models/note.server";
+
+export async function loader({ request }: LoaderArgs) {
+  const userId = await requireUserId(request);
+  const noteListItems = await getNoteListItems({ userId });
+  return json({ noteListItems });
+}
+
+export default function NotesPage() {
+  const data = useLoaderData<typeof loader>();
+  const user = useUser();
+
+  return (
+    <div className="flex h-full min-h-screen flex-col">
+      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+        <h1 className="text-3xl font-bold">
+          <Link to=".">Notes</Link>
+        </h1>
