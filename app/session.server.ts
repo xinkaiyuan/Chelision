45af14663df38c857e485e25e10011d637f6efca
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-07-12 16:01:56 +0000

    oprofile, s390: Fix uninitialized memory access when writing to oprofilefs

--- app/session.server.ts
+++ app/session.server.ts
@@ -7,4 +7,8 @@ import { getUserById } from "~/models/user.server";
 invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
 
 export const sessionStorage = createCookieSessionStorage({
-  co
+  cookie: {
+    name: "__session",
+    httpOnly: true,
+    path: "/",
+    sameSi
