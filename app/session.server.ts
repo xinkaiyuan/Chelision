dcc5240bcd6f93d127b90bec4570137901096ca4
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-07-12 07:06:47 +0000

    Minor changes to README

--- app/session.server.ts
+++ app/session.server.ts
@@ -6,4 +6,5 @@ import { getUserById } from "~/models/user.server";
 
 invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
 
-ex
+export const sessionStorage = createCookieSessionStorage({
+  co
