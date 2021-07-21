b54d4269044475da4a39c4fdd59cc48b412d78dc
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-07-21 07:18:58 +0000

    Update javascript.md

--- app/session.server.ts
+++ app/session.server.ts
@@ -19,4 +19,6 @@ export const sessionStorage = createCookieSessionStorage({
 
 const USER_SESSION_KEY = "userId";
 
-export async function getSession(request: 
+export async function getSession(request: Request) {
+  const cookie = request.headers.get("Cookie");
+  retur
