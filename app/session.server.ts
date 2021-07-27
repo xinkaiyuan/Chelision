ed69cf150792ea078c973095a8c9f9f95bc2b893
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-07-27 18:47:49 +0000

    add a gitignore file

--- app/session.server.ts
+++ app/session.server.ts
@@ -28,4 +28,8 @@ export async function getUserId(
   request: Request
 ): Promise<User["id"] | undefined> {
   const session = await getSession(request);
-  const userId = session.
+  const userId = session.get(USER_SESSION_KEY);
+  return userId;
+}
+
+export asyn
