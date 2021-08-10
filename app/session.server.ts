994cfec2f460e8d02b70ac29ae159b0d38d2c0cb
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-08-10 08:52:43 +0000

    Reformatting code

--- app/session.server.ts
+++ app/session.server.ts
@@ -51,4 +51,7 @@ export async function requireUserId(
     const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
     throw redirect(`/login?${searchParams}`);
   }
-  
+  return userId;
+}
+
+export async function requireUser(re
