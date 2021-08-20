d0d692354f09cfe4ecc346dc8844ae70ce2982b6
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-08-20 21:53:03 +0000

    Updated changelog for release

--- app/utils.ts
+++ app/utils.ts
@@ -1,4 +1,6 @@
 import { useMatches } from "@remix-run/react";
 import { useMemo } from "react";
 
-import type { User } from "~/mod
+import type { User } from "~/models/user.server";
+
+const DEFAULT_REDIRECT =
