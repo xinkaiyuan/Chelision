a6b98173c5a8492e91efe58d22cdf8d3a5489068
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-09-21 06:10:57 +0000

    Change Readme.md

--- app/utils.ts
+++ app/utils.ts
@@ -38,3 +38,5 @@ export function useMatchesData(
 ): Record<string, unknown> | undefined {
   const matchingRoutes = useMatches();
   const route = useMemo(
+    () => matchingRoutes.find((route) => route.id === id),
+    [
