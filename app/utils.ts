07f1200efe07a2ce848a8a7cfcb32a94b5fa98a4
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2021-10-20 10:45:10 +0000

    Added js files

--- app/utils.ts
+++ app/utils.ts
@@ -67,4 +67,5 @@ export function useUser(): User {
 }
 
 export function validateEmail(email: unknown): email is string {
-  return typeof email === "string" && email.length > 3 && email.incl
+  return typeof email === "string" && email.length > 3 && email.includes("@");
+}
