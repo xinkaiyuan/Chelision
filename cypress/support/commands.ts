69aeaf0746c812e487b4a13f3b6603ebb4966e6a
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2022-02-14 13:29:56 +0000

    Actualizar README

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -82,4 +82,5 @@ function deleteUserByEmail(email: string) {
 
 // We're waiting a second because of this issue happen randomly
 // https://github.com/cypress-io/cypress/issues/7306
-// Also added custom types to avoid getting de
+// Also added custom types to avoid getting detached
+// https://github.com/cypress-io/cypress/issues/
