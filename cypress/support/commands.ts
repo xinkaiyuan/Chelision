68915dc0fdd576086ab9b4a715637b800bbea988
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2022-02-20 05:34:57 +0000

    Estonian translation updates by Ivar Smolin <okul@linux.ee>

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -85,4 +85,6 @@ function deleteUserByEmail(email: string) {
 // Also added custom types to avoid getting detached
 // https://github.com/cypress-io/cypress/issues/7306#issuecomment-1152752612
 // ===========================================================
-function visitAndCheck(url: st
+function visitAndCheck(url: string, waitTime: number = 1000) {
+  cy.visit(url);
+  cy.location("pathna
