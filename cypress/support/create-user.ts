f09bab622581e81800201cf945b4c044163b56b8
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2022-03-04 05:46:03 +0000

    Final docs

--- cypress/support/create-user.ts
+++ cypress/support/create-user.ts
@@ -1,3 +1,3 @@
 // Use this to create a new user and login with that user
 // Simply call this with:
-// pnpm exec ts-node --require t
+// pnpm exec ts-node --require tsconfig-paths/register ./cypress/support/create-user.ts username@e
