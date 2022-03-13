afe5809a03fd94fc624afa8b0677b62b92c4495f
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2022-03-13 08:55:02 +0000

    Fix unittests

--- cypress/support/create-user.ts
+++ cypress/support/create-user.ts
@@ -1,3 +1,4 @@
 // Use this to create a new user and login with that user
 // Simply call this with:
-// pnpm exec ts-node --require tsconfig-paths/register ./cypress/support/create-user.ts username@e
+// pnpm exec ts-node --require tsconfig-paths/register ./cypress/support/create-user.ts username@example.com
+// and it will log out the cookie value you can use to in
