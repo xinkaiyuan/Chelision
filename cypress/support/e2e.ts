33a9b7f4cad8a1b25363119749c029e258a07192
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2022-04-27 09:40:29 +0000

    fixed eslint

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -2,4 +2,5 @@ import "@testing-library/cypress/add-commands";
 import "./commands";
 
 Cypress.on("uncaught:exception", (err) => {
-  // Cypress and React Hydrating the doc
+  // Cypress and React Hydrating the document don't get along
+  // for some unknown reason. Hopefu
