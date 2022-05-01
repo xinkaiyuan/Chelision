1a21e4b71f2387c21159950cef6f13e90eb03ffa
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2022-05-01 11:56:07 +0000

    imports

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -5,3 +5,6 @@ Cypress.on("uncaught:exception", (err) => {
   // Cypress and React Hydrating the document don't get along
   // for some unknown reason. Hopefully we figure out why eventually
   // so we can remove this.
+  if (
+    /hydrat/i.test(err.message) ||
+    /Minified React erro
