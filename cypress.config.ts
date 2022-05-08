582c695ef727872e468a2a4cf493a68487a3b8c3
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2022-05-08 17:51:52 +0000

    hmpf

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents: (on, config) => {
-      const isDev 
+      const isDev = config.watchForFileChanges;
+      const port = process.env.PORT
