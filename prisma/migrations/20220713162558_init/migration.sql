8961513ec007ffabbe3a0692d2a49982eb757c53
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2023-02-07 11:04:18 +0000

    Bleh

--- prisma/migrations/20220713162558_init/migration.sql
+++ prisma/migrations/20220713162558_init/migration.sql
@@ -21,4 +21,8 @@ CREATE TABLE "Note" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     "userId" TEXT NOT NULL,
-    CONSTRAINT "Note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCA
+    CONSTRAINT "Note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Use
