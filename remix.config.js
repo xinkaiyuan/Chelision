f559645dc526b151645adc3fea24e75bec841493
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2023-03-17 16:00:34 +0000

    more todos

--- remix.config.js
+++ remix.config.js
@@ -2,4 +2,8 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  cacheDirectory: "./node_module
+  cacheDirectory: "./node_modules/.cache/remix",
+  future: {
+    v2_meta: true,
+  },
+  ignoredR
