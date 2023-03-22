ce88c52577f8e36283c3510784fa1310c5ec53bb
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2023-03-22 10:01:25 +0000

    Travis-CI integration

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 
 # This file is how Fly starts the server (configured in fly.toml). Before starting
 # the server though, we need to run any prisma migrations that haven't yet been
-# run, which is why
+# run, which is why this file exists in the first place.
+# Learn more: https://co
