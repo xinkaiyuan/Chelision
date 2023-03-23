f6c7850bdb39351437a16baba8f985ff75aabb41
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2023-03-23 09:45:23 +0000

    v1.0.2

--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@
 # This file is how Fly starts the server (configured in fly.toml). Before starting
 # the server though, we need to run any prisma migrations that haven't yet been
 # run, which is why this file exists in the first place.
-# Learn more: https://co
+# Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
+
+set -ex
+pnpx
