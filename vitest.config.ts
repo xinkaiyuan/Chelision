e3a81a23bb234ed5d127436532ae1c9e1d5ae9fc
xinkaiyuan <92672360+xinkaiyuan@users.noreply.github.com>
2023-03-27 12:35:37 +0000

    qseecom: change qseecom bus scaling timer operation

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,4 +3,7 @@
 
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
-import tsconfigPaths from "vit
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [r
